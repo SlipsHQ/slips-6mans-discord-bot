a6a87f50de074abb9cef04cf331482d5864ccbe7
Husain Fazel <husain@slips.io>
2022-07-23 23:30:27 +0700

    Add jq/lq queue listings when no identifier is provided

--- commands/queue/joinQueue.js
+++ commands/queue/joinQueue.js
@@ -24,13 +24,68 @@ module.exports = {
         	return;
 	    }
 
+	    let queueIdentifier = null;
+
 	    if(args.length < 1) {
-	    	await message.reply("You need to provide an identifier for the queue you want to join");
+
+	    	// get queues
+	    	const whereClause = {
+	    		status: {
+	    			[Op.in]: ['created', 'in_progress']
+	    		}
+	    	}
+
+		    const allQueues = await models.Queue.findAll({
+		    	where: whereClause
+		    });
+
+	    	let queueId = null;
+		    let queueMessage = '';
+
+		    for(var q of allQueues) {
+		    	queueMessage += `[${q.id}]: ${q.identifier}\n`;
+		    }
+
+		    if(!allQueues.length) {
+		    	queueMessage = '*There are no open queues at the moment*';
+		    }
+
+	    	try {
+	  			await message.channel.send(`** Choose the ID of the queue you want to join**\n${queueMessage}`);
+	  			if(!allQueues.length) {
+					return;
+				}
+				await message.channel.awaitMessages((m) => message.author.id === m.author.id, { max: 1, time: 300000, errors: ['time'] })
+					.then(async collected => {
+						queueId = parseInt(collected.first().content);
+					})
+					.catch(collected => {
+						if(collected === error) {
+							throw "error";
+						} else {
+							message.channel.send('Sorry, you did not respond in time! :cry:');
+							return;
+						}
+					});
+
+
+			} catch(e) {
+				await message.channel.send('Please try again.');
+				return;
+			}
+
+			queueIdentifier = allQueues.filter(queue => queue.id == queueId)[0]?.identifier;
+
+	    } else {
+	    	queueIdentifier = args[0];
+	    }
+
+	    if(!queueIdentifier) {
+	    	await message.reply("No queue identifier was provided!");
         	return;
 	    }
 
 	    // get the queue
-		const queueIdentifier = args[0];
 	    const queue = await models.Queue.findOne({ where: { identifier: queueIdentifier }});
 	    if(!queue) {
 	    	await message.reply("Cannot find the queue with the identifier you provided, try again!");
--- commands/queue/leaveQueue.js
+++ commands/queue/leaveQueue.js
@@ -24,13 +24,71 @@ module.exports = {
         	return;
 	    }
 
+	    let queueIdentifier = null;
+
 	    if(args.length < 1) {
-	    	await message.reply("You need to provide an identifier for the queue you want to join");
+	    	
+	    	const allQueuesJoined = await models.QueueParticipant.findAll({ where: { player_id: player.id }});
+	    	const allQueueIds = allQueuesJoined.map(queueParticipant => queueParticipant.queue_id);
+
+	    	const allQueues = await models.Queue.findAll({
+	    		where: {
+				    id: {
+				      	[Op.in]: allQueueIds
+				    },
+				    status: {
+	    				[Op.in]: ['created']
+	    			}
+				}
+	    	});
+
+	    	let queueId = null;
+		    let queueMessage = '';
+
+		    for(var q of allQueues) {
+		    	queueMessage += `[${q.id}]: ${q.identifier}\n`;
+		    }
+
+		    if(!allQueues.length) {
+		    	queueMessage = `*You haven't joined any queues that haven't started/been completed yet*`;
+		    }
+
+	    	try {
+	  			await message.channel.send(`** Choose the ID of the queue you want to leave**\n${queueMessage}`);
+	  			if(!allQueues.length) {
+					return;
+				}
+				await message.channel.awaitMessages((m) => message.author.id === m.author.id, { max: 1, time: 300000, errors: ['time'] })
+					.then(async collected => {
+						queueId = parseInt(collected.first().content);
+					})
+					.catch(collected => {
+						if(collected === error) {
+							throw "error";
+						} else {
+							message.channel.send('Sorry, you did not respond in time! :cry:');
+							return;
+						}
+					});
+
+
+			} catch(e) {
+				await message.channel.send('Please try again.');
+				return;
+			}
+
+			queueIdentifier = allQueues.filter(queue => queue.id == queueId)[0]?.identifier;
+
+	    } else {
+	    	queueIdentifier = args[0];
+	    }
+
+	    if(!queueIdentifier) {
+	    	await message.reply("No queue identifier was provided!");
         	return;
 	    }
 
 	    // get the queue
-		const queueIdentifier = args[0];
 	    const queue = await models.Queue.findOne({ where: { identifier: queueIdentifier }});
 	    if(!queue) {
 	    	await message.reply("Cannot find the queue with the identifier you provided, try again!");
